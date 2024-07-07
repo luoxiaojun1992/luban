700ca50145a9066da994d0094aef2c4eedebdbda
罗晓俊 <luoxiaojun1992@sina.cn>
2024-07-07 14:37:54 +0800

    feat: update sql ast condition.go

--- engine/ast/sql/elements/condition.go
+++ engine/ast/sql/elements/condition.go
@@ -1,9 +1,31 @@
 package elements
 
-import lubanSQL "github.com/luoxiaojun1992/luban/engine/ast/sql"
+import (
+	"fmt"
+	"strings"
+	
+	lubanSQL "github.com/luoxiaojun1992/luban/engine/ast/sql"
+)
 
 type Condition struct {
 	LeftOperand   lubanSQL.IOperand
 	Operator      string
 	RightOperands []lubanSQL.IOperand
+}
+
+func (c *Condition) ToRaw() string {
+	sqlTpl := "%s %s %s"
+
+	rightSQLTpl := "%s"
+	if len(c.RightOperands) > 1 {
+		rightSQLTpl = "(" + rightSQLTpl + ")"
+	}
+	
+	var rightOperandSQLList []string
+	for _, rightOperand := range c.RightOperands {
+		rightOperandSQLList = append(rightOperandSQLList, rightOperand.ToRaw())
+	}
+	rightSQL := fmt.Sprintf(rightSQLTpl, strings.Join(rightOperandSQLList, ", "))
+
+	return fmt.Sprintf(sqlTpl, c.LeftOperand.ToRaw(), c.Operator, rightSQL)
 }
